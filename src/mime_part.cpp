7ce5180a301e3d8a2ee6385c3034f0fcb33c181c
Leo <leo.ghafari+github2@gmail.com>
2018-06-07 18:11:58 +0900

    The curl mime part setting now throws when an issue occurs instead of being silent

--- src/mime_part.cpp
+++ src/mime_part.cpp
@@ -1,6 +1,7 @@
 #include "mime_part.hpp"
 
 #include <string>
+#include <stdexcept>
 
 #include <curl/curl.h>
 
@@ -30,24 +31,45 @@ yuki::http::file_data_part::file_data_part(const std::string& name, const std::s
 void yuki::http::part::set_up_handle(curl_mime* mime_handle) const
 {
 	curl_mimepart* handle = curl_mime_addpart(mime_handle);
-	curl_mime_name(handle, m_name.c_str());
+	if(not handle)
+	{
+		throw std::runtime_error("Impossible to add a part to the curl mime object");
+	}
+
+	if(auto err = curl_mime_name(handle, m_name.c_str()); err != CURLE_OK)
+	{
+		throw std::runtime_error("Impossible to set the curl mime part name. Error : " + std::to_string(err));
+	}
 	set_up_handle_impl(handle);
 }
 
 
 void yuki::http::file_part::set_up_handle_impl(curl_mimepart* handle) const
 {
-	curl_mime_filedata(handle, m_file_name.c_str());
+	if(auto err = curl_mime_filedata(handle, m_file_name.c_str()); err != CURLE_OK)
+	{
+		throw std::runtime_error("Impossible to set the curl mime part filedata. Error : " + std::to_string(err));
+	}
 }
 
 
 void yuki::http::file_data_part::set_up_handle_impl(curl_mimepart* handle) const
 {
-	curl_mime_data(handle, m_file_content.data(), m_file_content.size());
-	curl_mime_filename(handle, m_file_name.c_str());
+	if(auto err = curl_mime_data(handle, m_file_content.data(), m_file_content.size()); err != CURLE_OK)
+	{
+		throw std::runtime_error("Impossible to set the curl mime part file data. Error : " + std::to_string(err));
+	}
+
+	if(auto err = curl_mime_filename(handle, m_file_name.c_str()); err != CURLE_OK)
+	{
+		throw std::runtime_error("Impossible to set the curl mime part file name. Error : " + std::to_string(err));
+	}
 }
 
 void yuki::http::raw_part::set_up_handle_impl(curl_mimepart* handle) const
 {
-	curl_mime_data(handle, m_data.data(), m_data.size());
+	if(auto err = curl_mime_data(handle, m_data.data(), m_data.size()); err != CURLE_OK)
+	{
+		throw std::runtime_error("Impossible to set the curl mime part raw data. Error : " + std::to_string(err));
+	}
 }
